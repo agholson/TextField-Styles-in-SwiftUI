194243466e4293e3680d51278a502afbf6875d83
agholson <87455899+agholson@users.noreply.github.com>
2022-04-03 09:16:34 -0400

    Initial Commit

---
+++ M1L5StylingTextFields/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M1L5StylingTextFields
+//
+//  Created by Leone on 4/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M1L5StylingTextFields/M1L5StylingTextFieldsApp.swift
@@ -0,0 +1,17 @@
+//
+//  M1L5StylingTextFieldsApp.swift
+//  M1L5StylingTextFields
+//
+//  Created by Leone on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct M1L5StylingTextFieldsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
